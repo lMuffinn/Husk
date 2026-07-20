6f899d3c6227639f52ded23c325a4c6a22661b09
lMuffinn <mattheweagleman@gmail.com>
2026-07-20 12:41:40 -0500

    Changed a lot about the structure
    - created characters folder
    - started seperating floors so they are easier to work on

--- Assets/Scripts/Characters/Older Brother/Movement.cs
+++ Assets/Scripts/Characters/Older Brother/Movement.cs
@@ -25,6 +25,7 @@ public class Movement : MonoBehaviour
     [SerializeField] float _accelerate = 5f;
     [SerializeField] float _decelerate = 7f;
 
+    //Normal Movement
     Vector2 _moveInput;
     float _speed;
     float _velocityX;
@@ -34,27 +35,9 @@ public class Movement : MonoBehaviour
 
 
 
+    //stairs-------------------
     bool _isOnStairs = false;
     bool _stairsFaceRight;
-
-    // float speed = 10;
-    // public LayerMask up;
-    // public LayerMask down;
-    // public bool isTouchingStairs = false;
-    // Vector3 pos;
-    //interaction--------------
-    // public float interactionRadius;
-    // public LayerMask Interactable;
-    // //stairs-------------------
-    // public float stairsRadius = .2f;
-    // public LayerMask stairsRight;
-    // public LayerMask stairsLeft;
-    // float stairsBonus;
-    // public Transform feet;
-    // public int floor;
-    //camera--------------------
-    // public Collider2D[] boundary;
-    // public GameObject cinemachine;
 
     void Awake()
     {
@@ -62,24 +45,14 @@ public class Movement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         _sortingGroup = GetComponent<SortingGroup>();
         _playerInput = GetComponent<PlayerInput>();
+
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        /* The following code is old and bad. It made it so when the player goes onto the stairs they automatically start moving 
-         * diagonally as to look like they are actaully going up/down the stairs.
-        //is the player on stairs
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        bool left = Physics2D.OverlapCircle(feet.position, stairsRadius, stairsLeft);
-        bool right = Physics2D.OverlapCircle(feet.position, stairsRadius, stairsRight);
-        if (left) stairsBonus = horizontal * -1;
-        else if (right) stairsBonus = horizontal;
-        else stairsBonus = 0;
-        //controls movement + whatever direction the stairs are in
-        rb.linearVelocity = new Vector2(horizontal * speed, vertical * speed + speed * stairsBonus);
-        This can be removed when better code is added*/
+
+
 
         _moveInput = _playerInput.actions["Move"].ReadValue<Vector2>();
 
@@ -104,7 +77,7 @@ public class Movement : MonoBehaviour
     }
     void Update()
     {
-       // cinemachine.GetComponent<CinemachineConfiner2D>().m_BoundingShape2D = boundary[GetComponent<Floor>().floor];
+
     }
 
     public void Sprint(InputAction.CallbackContext context)
@@ -119,30 +92,8 @@ public class Movement : MonoBehaviour
 
     public void IsOnStairs(bool state, bool isRight)
     {
+        // set variables to track if the character is on the stairs or not
         _isOnStairs = state;
         _stairsFaceRight = isRight;
     }
-
-
-    // Transform GetClosestInteractable(List<Transform> interactableObjects, Transform fromThis)
-    // {
-    //     //This is old lame muffin coding lol. I got this off the internet so its not even mine
-    //     //All it does is get the position of the nearest object? Its so simple its kinda funny i had to look it up.
-    //     //This was being used in my implementation of interactable objects
-    //     Transform bestTarget = null;
-    //     float closestDistanceSqr = Mathf.Infinity;
-    //     Vector3 currentPosition = fromThis.position;
-    //     foreach (Transform potentialTarget in interactableObjects)
-    //     {
-    //         Vector3 directionToTarget = potentialTarget.position - currentPosition;
-    //         float dSqrToTarget = directionToTarget.sqrMagnitude;
-    //         if (dSqrToTarget < closestDistanceSqr)
-    //         {
-    //             closestDistanceSqr = dSqrToTarget;
-    //             bestTarget = potentialTarget;
-    //         }
-    //     }
-    //     return bestTarget;
-    // }
-
 }
